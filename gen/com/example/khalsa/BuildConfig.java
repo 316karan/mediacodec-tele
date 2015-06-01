56f643d423b536127665dea3c452f9f790fd10c4
316karan <karan@karan-Vostro-2520.(none)>
2015-06-01 17:13:16 +0530

    first commit

---
+++ gen/com/example/khalsa/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.khalsa;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/khalsa/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.khalsa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.khalsa.video.MediaController;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		MediaController mc = new MediaController(this);
+		mc.convertVideo("/sdcard/DCIM/V_20150528_134216.mp4");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
